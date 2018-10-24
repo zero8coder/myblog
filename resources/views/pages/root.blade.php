f751d399c360aba2e50470828cf62276af8c0a96
Zero <s812419396@gmail.com>
2018-10-24 07:56:54 +0000

    Finsh index page

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.app')
 @section('title', '首页')
 @section('content')
-    <h1>首页</h1>
+    <div class="col-lg-9 col-md-9 page-list">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <ul class="media-list">
+                    <li class="media">
+                        <a href="#">
+                            <div class="media-left">教程1</div>
+                            <div class="media-right">2018-10-20</div>
+                        </a>
+                    </li>
+                    <hr>
+                    <li class="media">
+                        <a href="#">
+                            <div class="media-left">教程2</div>
+                            <div class="media-right">2018-10-20</div>
+                        </a>
+                    </li>
+                    <hr>
+                     <li class="media">
+                        <a href="#">
+                            <div class="media-left">教程2</div>
+                            <div class="media-right">2018-10-20</div>
+                        </a>
+                    </li>
+                    <hr>
+                </ul>
+            </div>
+        </div>
+    </div>
 @stop
