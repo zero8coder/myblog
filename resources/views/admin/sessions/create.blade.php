88e7ea6e190f79fcefbe2ad79544ab4ec700d61c
ZeroXGod <s812419396@gmail.com>
2018-11-16 02:07:45 +0000

    调整后台登录页样式

--- resources/views/admin/sessions/create.blade.php
+++ resources/views/admin/sessions/create.blade.php
@@ -1,38 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>登录</title>
+    <title>启动</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <div id="app" class="admin-{{ route_class() }}-page">
+    <div id="app" class="{{ route_class() }}-page">
         <div class="container">
             <div class="col-md-offset-1 col-md-10">
-                <br>
                 <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h5>登录</h5>
-                    </div>
                     <div class="panel-body">
 
                         <form method="POST" action="">
                             {{ csrf_field() }}
 
                             <div class="form-group">
-                                <label for="email">邮箱：</label>
-                                <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                                <input type="text" name="email" class="form-control" value="{{ old('email') }}" placeholder="email">
                             </div>
 
                             <div class="form-group">
-                                <label for="password">密码:</label>
-                                <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                                <input type="password" name="password" class="form-control" value="{{ old('password') }}" placeholder="password">
                             </div>
 
-                            <div class="form-group">
+                         {{--    <div class="form-group">
                                 <label><input type="checkbox" name="remember">记住我</label>
-                            </div>
+                            </div> --}}
 
-                            <button type="submit" class="btn btn-primary">登录</button>
+                            <button type="submit" class="btn btn-primary">GO</button>
                         </form>
 
                         <hr>
