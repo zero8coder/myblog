4f37cc99238902a864bc1f91967378bf3200c757
ZeroXGod <s812419396@gmail.com>
2018-11-30 06:38:04 +0000

    添加文章删除确认框

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -14,11 +14,11 @@
                         <input name="" type="button" class="edit-img-btn" />
                     </a>
                     &nbsp;
-                    <form action="{{ route('admin.articles.destroy', $article->id) }}" method="post" style="display:inline">
+                    <form id="delForm" action="{{ route('admin.articles.destroy', $article->id) }}" method="post" style="display:inline">
                         {{ csrf_field() }}
                         {{ method_field('DELETE') }}
 
-                        <input name="" type="submit" class="del-img-btn"  value=''/>
+                        <input class="del-img-btn" data-toggle="modal" data-target="#delModel"/>
                     </form>
                 </td>
                 <td>{{ $article->category->name }}</td>
@@ -34,7 +34,48 @@
         @endforeach
     </table>
 </div>
+
 <div class="text-center">
     {!! $articles->render() !!}
 </div>
+
+<!-- 删除文章模态框（Modal） -->
+<div class="modal fade" id="delModel" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                    &times;
+                </button>
+                <h4 class="modal-title" id="modalLabel">
+                    删除
+                </h4>
+            </div>
+            <div class="modal-body">
+                是否删除该文章
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" onclick="delArticle()">
+                    <span class="glyphicon glyphicon-ok" style="color:#66CD00"></span>
+                </button>
+                <button type="button" class="btn btn-default" data-dismiss="modal" >
+                    <span class="glyphicon glyphicon-remove" style="color:red"></span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
 @stop
+
+@section('scripts')
+    <script>
+        function delArticle()
+        {
+            $('#delModel').modal('hide')
+            $('#delForm').submit();
+        }
+    </script>
+@stop
+
+
