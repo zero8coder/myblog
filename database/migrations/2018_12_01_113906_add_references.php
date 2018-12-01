e44181419f4d296a16a9d2bd71b229c4405f38d8
Zero <s812419396@gmail.com>
2018-12-01 03:48:23 +0000

    文章分类添加外籍约束

---
+++ database/migrations/2018_12_01_113906_add_references.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddReferences extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            // 当 category_id 对应的 categories 表数据被删除时， 删除词条
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            // 移除外键约束
+            $table->dropForeign(['category_id']);
+        });
+    }
+}
